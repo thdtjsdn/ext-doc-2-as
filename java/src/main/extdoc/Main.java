5ff7bd7983be3c9ee09d881eda4d93ba9c53f8a5
oxxxymoron <oxxxymoron@4a32c7fa-a550-11dd-a8c6-677c70d10fe4>
2008-12-25 10:21:34 +0000

    cli

--- java/src/main/extdoc/Main.java
+++ java/src/main/extdoc/Main.java
@@ -1,6 +1,7 @@
 package extdoc;
 
 import extdoc.jsdoc.processor.FileProcessor;
+import org.apache.commons.cli.*;
 
 /**
  * User: Andrey Zubkov
@@ -9,12 +10,44 @@ import extdoc.jsdoc.processor.FileProcessor;
  */
 
 public class Main {
+
+    private static Options options = new Options();
+
+    private static void wrongCli(String msg){
+        System.err.println("Wrong command line arguments: "+ msg);
+        showHelp();
+    }
+
+    private static void showHelp(){
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp( "java -jar ext-doc.jar", options);
+    }
+
     public static void main(String[] args) {
-        String xmlFileName = args[0];
-        String outputFolderName = args[1];
-        String templateFileName = args[2];
-        FileProcessor processor = new FileProcessor();
-        processor.process(xmlFileName);
-        processor.saveToFolder(outputFolderName, templateFileName);
+
+        options = new Options();
+        options.addOption("h","help", false, "Show detailed help.");        
+        options.addOption("p","project", true, "Project XML file.");
+        options.addOption("o","output", true, "Directory where documentation should be created.");
+        options.addOption("t","template", true, "XML File containing template informaiton.");
+
+        CommandLineParser parser = new PosixParser();
+        try {
+            CommandLine cmd = parser.parse( options, args);
+            if(cmd.hasOption("help")){
+                showHelp();
+            }else if(cmd.hasOption("project") &&
+                    cmd.hasOption("output") &&
+                    cmd.hasOption("template")){
+                FileProcessor processor = new FileProcessor();
+                processor.process(cmd.getOptionValue("project"));
+                processor.saveToFolder(cmd.getOptionValue("output"),
+                        cmd.getOptionValue("template"));
+            }else{
+                wrongCli("required arguments missing");                                
+            }
+        } catch (ParseException e) {
+            wrongCli(e.getMessage());
+        }
     }
 }
--- sample/ext-doc.bat
+++ sample/ext-doc.bat
@@ -1,1 +1,1 @@
-java -jar ../ext-doc.jar ext.xml ../output ../template/ext/template.xml
+java -jar ../ext-doc.jar -p ext.xml -o ../output -t ../template/ext/template.xml
